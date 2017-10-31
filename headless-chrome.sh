4d8b0cca325248727b42a5f710fc5d5f0dcfaec1
Ryuichi Tokugami <ryuichi.tokugami@plan-b.co.jp>
2017-11-01 08:44:45 +0900

    scriptなおした

--- headless-chrome.sh
+++ headless-chrome.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 URL=$1
+Z=$2
+
+if [ "$URL" == ""]
+then
+  echo "*URL REQUIRE"
+  exit
+fi
+if [ "$Z" == "" ]
+then
+  Z="http_req,tree"
+fi
+
+
 
 # tcpdump dst port 80 or port 443 & 
 #tcpdump -ttt -n -s 0 -w - | grep -a -o -E "Host\: .*|GET \/.*|POST \/.*" 2>/dev/null &
-tshark -w packet
+tshark -w packet &
 
 PID=$!
 chromium-browser --headless --no-sandbox --disable-gpu "${URL}" 2>/dev/null
